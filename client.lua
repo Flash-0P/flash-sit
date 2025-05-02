2475ed8f98d0f461d4401b7cd4a01b9681c2a379
Flash OP <128111282+Flash-0P@users.noreply.github.com>
2025-05-02 02:05:09 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -29,6 +29,7 @@ local sittingScenarios = {
 }
 
 -- Functions --
+-- flash
 local function DisplayNativeNotification(msg)
 	BeginTextCommandThefeedPost("STRING")
 	AddTextComponentSubstringPlayerName(msg)
